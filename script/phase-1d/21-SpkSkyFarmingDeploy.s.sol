26a27074e19b9c6ec6ee035b7544764ce9da0500
amusingaxl <112016538+amusingaxl@users.noreply.github.com>
2024-10-02 19:00:38 -0300

    feat: add SPK -> SKY farm scripts

---
+++ script/phase-1d/21-SpkSkyFarmingDeploy.s.sol
@@ -0,0 +1,76 @@
+// SPDX-FileCopyrightText: © 2023 Dai Foundation <www.daifoundation.org>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+pragma solidity ^0.8.16;
+
+import {Script} from "forge-std/Script.sol";
+import {ScriptTools} from "dss-test/ScriptTools.sol";
+
+import {Reader} from "../helpers/Reader.sol";
+import {StakingRewardsDeploy, StakingRewardsDeployParams} from "../dependencies/StakingRewardsDeploy.sol";
+import {
+    VestedRewardsDistributionDeploy,
+    VestedRewardsDistributionDeployParams
+} from "../dependencies/VestedRewardsDistributionDeploy.sol";
+
+contract Phase1d_SpkSkyFarmingDeployScript is Script {
+    ChainlogLike internal constant chainlog = ChainlogLike(0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F);
+
+    string internal constant NAME = "phase-1d/spk-sky-farming-deploy";
+
+    function run() external {
+        Reader reader = new Reader(ScriptTools.loadConfig());
+
+        address admin = chainlog.getAddress("MCD_PAUSE_PROXY");
+
+        address spk = reader.envOrReadAddress("FOUNDRY_SPK", ".spk");
+        address sky = reader.envOrReadAddress("FOUNDRY_SKY", ".sky");
+        address vest = reader.envOrReadAddress("FOUNDRY_VEST", ".vest");
+        address dist = reader.readAddressOptional(".dist");
+        address rewards = reader.readAddressOptional(".rewards");
+
+        vm.startBroadcast();
+
+        if (rewards == address(0)) {
+            rewards = StakingRewardsDeploy.deploy(
+                StakingRewardsDeployParams({owner: admin, stakingToken: spk, rewardsToken: sky})
+            );
+        }
+
+        if (dist == address(0)) {
+            dist = VestedRewardsDistributionDeploy.deploy(
+                VestedRewardsDistributionDeployParams({
+                    deployer: msg.sender,
+                    owner: admin,
+                    vest: vest,
+                    rewards: rewards
+                })
+            );
+        }
+
+        vm.stopBroadcast();
+
+        ScriptTools.exportContract(NAME, "admin", admin);
+        ScriptTools.exportContract(NAME, "spk", spk);
+        ScriptTools.exportContract(NAME, "sky", sky);
+        ScriptTools.exportContract(NAME, "dist", dist);
+        ScriptTools.exportContract(NAME, "rewards", rewards);
+        ScriptTools.exportContract(NAME, "vest", vest);
+    }
+}
+
+interface ChainlogLike {
+    function getAddress(bytes32 _key) external view returns (address addr);
+}
---
+++ script/phase-1d/22-SpkSkyFarmingInit.s.sol
@@ -0,0 +1,100 @@
+// SPDX-FileCopyrightText: © 2023 Dai Foundation <www.daifoundation.org>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+pragma solidity ^0.8.16;
+
+import {Script} from "forge-std/Script.sol";
+import {ScriptTools} from "dss-test/ScriptTools.sol";
+
+import {Reader} from "../helpers/Reader.sol";
+import {
+    SpkSkyFarmingInit,
+    SpkSkyFarmingInitParams,
+    SpkSkyFarmingInitResult
+} from "../dependencies/phase-1d/SpkSkyFarmingInit.sol";
+
+contract Phase1d_SpkSkyFarmingInitScript is Script {
+    using ScriptTools for string;
+
+    ChainlogLike internal constant chainlog = ChainlogLike(0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F);
+
+    string internal constant NAME = "phase-1d/spk-sky-farming-init";
+
+    function run() external {
+        Reader config = new Reader(ScriptTools.loadConfig());
+        Reader deps = new Reader(ScriptTools.loadDependencies());
+
+        uint256 vestTot = config.envOrReadUint("FOUNDRY_VEST_TOT", ".vestTot");
+        uint256 vestBgn = config.envOrReadUint("FOUNDRY_VEST_BGN", ".vestBgn");
+        uint256 vestTau = config.envOrReadUint("FOUNDRY_VEST_TAU", ".vestTau");
+
+        address spk = deps.envOrReadAddress("FOUNDRY_SPK", ".spk");
+        address sky = deps.envOrReadAddress("FOUNDRY_SKY", ".sky");
+        address dist = deps.envOrReadAddress("FOUNDRY_DIST", ".dist");
+        address rewards = deps.envOrReadAddress("FOUNDRY_FARM", ".rewards");
+        address vest = deps.envOrReadAddress("FOUNDRY_VEST", ".vest");
+
+        SpkSkyFarmingInitParams memory farmingCfg = SpkSkyFarmingInitParams({
+            sky: sky,
+            spk: spk,
+            dist: dist,
+            distKey: "REWARDS_DISTRIBUTION_SPK_SKY",
+            rewards: rewards,
+            rewardsKey: "FARM_SPK_SKY",
+            vest: vest,
+            vestTot: vestTot,
+            vestBgn: vestBgn,
+            vestTau: vestTau
+        });
+
+        address pauseProxy = chainlog.getAddress("MCD_PAUSE_PROXY");
+
+        vm.startBroadcast();
+
+        SpkSkyFarmingInitSpell spell = new SpkSkyFarmingInitSpell();
+        bytes memory out = ProxyLike(pauseProxy).exec(address(spell), abi.encodeCall(spell.cast, (farmingCfg)));
+
+        vm.stopBroadcast();
+
+        SpkSkyFarmingInitResult memory res = abi.decode(out, (SpkSkyFarmingInitResult));
+
+        ScriptTools.exportContract(NAME, "sky", sky);
+        ScriptTools.exportContract(NAME, "dist", dist);
+        ScriptTools.exportContract(NAME, "rewards", rewards);
+        ScriptTools.exportContract(NAME, "vest", vest);
+        ScriptTools.exportValue(NAME, "vestId", res.vestId);
+    }
+}
+
+contract SpkSkyFarmingInitSpell {
+    uint256 internal constant CAP = type(uint256).max;
+
+    function cast(SpkSkyFarmingInitParams memory farmingCfg) public returns (SpkSkyFarmingInitResult memory) {
+        DssVestLike(farmingCfg.vest).file("cap", CAP);
+        return SpkSkyFarmingInit.init(farmingCfg);
+    }
+}
+
+interface ProxyLike {
+    function exec(address usr, bytes memory fax) external returns (bytes memory out);
+}
+
+interface ChainlogLike {
+    function getAddress(bytes32 _key) external view returns (address addr);
+}
+
+interface DssVestLike {
+    function file(bytes32 _what, uint256 _data) external;
+}
